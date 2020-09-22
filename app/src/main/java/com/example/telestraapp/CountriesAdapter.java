013bc3b4982ae69abf052e311dc110ba68ac0868
abdul <abdul.tanveer@gmail.com>
2020-09-22 13:02:45 +0530

    recyclerview with adapter -- instruction nos

--- app/src/main/java/com/example/telestraapp/CountriesAdapter.java
+++ app/src/main/java/com/example/telestraapp/CountriesAdapter.java
@@ -9,41 +9,41 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.CountryViewHolder> {
+public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.CountryViewHolder> {//4
 
-String[] mCountries;
-    private LayoutInflater mInflater;
+String[] mCountries;//8
+    private LayoutInflater mInflater;//10
 
     public CountriesAdapter(String[] countries, Context context) {
-        mCountries = countries;
-        mInflater = LayoutInflater.from(context);
+        mCountries = countries;//8a
+        mInflater = LayoutInflater.from(context);//11
     }
 
     @NonNull
     @Override //onCreateViewHolder --- to create/buy new  plank-strip [
-    public CountryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View mItemView = mInflater.inflate(R.layout.recyclerlist_row, parent, false);
-        return new CountryViewHolder(mItemView);
+    public CountryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {//6
+        View mItemView = mInflater.inflate(R.layout.recyclerlist_row, parent, false);//12
+        return new CountryViewHolder(mItemView);//13
     }
 
     @Override//onBindViewHolder -- write the data on to the newly created/recycled  plank
-    public void onBindViewHolder(@NonNull CountryViewHolder holder, int position) {
-        holder.countryText.setText(mCountries[position]);  //binding the data onto the inflated plank
+    public void onBindViewHolder(@NonNull CountryViewHolder holder, int position) {//6
+        holder.countryText.setText(mCountries[position]);  //binding the data onto the inflated plank//15
     }
 
     @Override // getItemCount -- tell how many data items are there so that the above 2 guys can create or recycle items
-    public int getItemCount() {
-        return mCountries.length;
+    public int getItemCount() {//6
+        return mCountries.length;//9
     }
 
     /**
      * this guy will have a box of empty planks/ planks to recycle and insert at the bottom of the list/screen
      */
-    public class CountryViewHolder extends RecyclerView.ViewHolder {
-       public TextView countryText;
+    public class CountryViewHolder extends RecyclerView.ViewHolder {//5
+       public TextView countryText;//14
         public CountryViewHolder(@NonNull View itemView) {
             super(itemView);
-            countryText =  itemView.findViewById(R.id.textViewCountry);
+            countryText =  itemView.findViewById(R.id.textViewCountry);//16
         }
     }
 }
--- app/src/main/java/com/example/telestraapp/RecyclerActivity.java
+++ app/src/main/java/com/example/telestraapp/RecyclerActivity.java
@@ -7,18 +7,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 public class RecyclerActivity extends AppCompatActivity {
-    String[] countries = new String[]{"india","usa","uk"};
+    String[] countries = new String[]{"india","usa","uk"}; //1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
-        RecyclerView countriesRecyclerView = findViewById(R.id.recyclerViewCountries);
+        RecyclerView countriesRecyclerView = findViewById(R.id.recyclerViewCountries);//3
 
-        CountriesAdapter adapter = new CountriesAdapter(countries,this);
+        CountriesAdapter adapter = new CountriesAdapter(countries,this);//7---10
 
-        countriesRecyclerView.setAdapter(adapter);
-        countriesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        countriesRecyclerView.setAdapter(adapter);//17
+        countriesRecyclerView.setLayoutManager(new LinearLayoutManager(this));//18
         //layout manager --- he's suppose to arrange the layout of list either horizontally or vertically or staggered
     }
 }
