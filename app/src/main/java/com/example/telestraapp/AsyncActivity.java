65140ec47181e792e5d3a5123ec4c4d84021218a
abdul <abdul.tanveer@gmail.com>
2020-09-24 12:14:21 +0530

    dynamic regn of broadcast receiver

--- app/src/main/java/com/example/telestraapp/AsyncActivity.java
+++ app/src/main/java/com/example/telestraapp/AsyncActivity.java
@@ -2,6 +2,7 @@ package com.example.telestraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -15,8 +16,8 @@ ProgressBar progressBar;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async);
         progressBar = findViewById(R.id.progressBardownload);
-
-
+        IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+        registerReceiver(new SmsReceiver(),filter); //dynamic broadcast receiver
 
     }
 
