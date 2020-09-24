069d1440bc52e9447ba5e3e6119d1d90c4c9c021
abdul <abdul.tanveer@gmail.com>
2020-09-24 10:34:13 +0530

    created a asynctask

---
+++ app/src/main/java/com/example/telestraapp/AsyncActivity.java
@@ -0,0 +1,27 @@
+package com.example.telestraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.example.telestraapp.background.DownloadTask;
+
+public class AsyncActivity extends AppCompatActivity {
+ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async);
+        progressBar = findViewById(R.id.progressBardownload);
+
+
+
+    }
+
+    public void taskClick(View view) {
+        DownloadTask downloadTask = new DownloadTask(progressBar);
+        downloadTask.execute("www.imageurltobedowloaded.com");
+    }
+}
