a8e4d308f0d1ea650ee83ebc3ac6a14576eec06b
abdul <abdul.tanveer@gmail.com>
2020-09-24 11:54:36 +0530

    SmsReceiver broadcast recieiver-- showing sms content in a  toast on recieve

--- app/src/main/java/com/example/telestraapp/SmsReceiver.java
+++ app/src/main/java/com/example/telestraapp/SmsReceiver.java
@@ -3,14 +3,47 @@ package com.example.telestraapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
 
 public class SmsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        final Bundle bundle = intent.getExtras();
 
-        Toast.makeText(context, "hey abdul you received an sms", Toast.LENGTH_SHORT).show();
+        try {
+
+            if (bundle != null) {
+
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusObj.length; i++) {
+
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+
+                    Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
+
+
+                    // Show alert
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context, "senderNum: "+ senderNum + ", message: " + message, duration);
+                    toast.show();
+
+                } // end for loop
+            } // bundle is null
+
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" +e);
+
+        }
+       // Toast.makeText(context, "hey abdul you received an sms", Toast.LENGTH_SHORT).show();
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
        // throw new UnsupportedOperationException("Not yet implemented");
